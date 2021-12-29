71b988003b13205b0aa13cbb7c63b6acc2108974
Leelavathi Gade <leelaroshangade@gmail.com>
2021-12-29 15:53:52 +0100

    pages added

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './react.svg';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <div className="Home-header">
+        <div>
+          <h1>Home Page</h1>
+          <h4>Click here to see counter component <Link to='/counter'>Counter</Link></h4>
+        </div>
+        {/* <div className="Home-header">
           <img src={logo} className="Home-logo" alt="logo" />
           <h2>Welcome to Razzle</h2>
         </div>
@@ -24,7 +29,7 @@ class Home extends React.Component {
           <li>
             <a href="https://palmer.chat">Community Slack</a>
           </li>
-        </ul>
+        </ul> */}
       </div>
     );
   }
